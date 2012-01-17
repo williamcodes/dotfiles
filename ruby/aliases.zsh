77d39884995a7b7b58462fff8556f1860d3323de
Dane O'Connor <dane.oconnor@gmail.com>
2012-01-17 12:02:56 -0500

    merge all zsh config into zsh directory and aliases.zsh

--- ruby/aliases.zsh
+++
@@ -1,9 +0,0 @@
-alias r='rvm use 1.8.7'
-
-alias f='RAILS_ENV=fi'
-
-alias sc='script/console'
-alias sg='script/generate'
-alias sd='script/destroy'
-
-alias migrate='rake db:migrate db:test:clone'
--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,2 +1,59 @@
 alias reload!='. ~/.zshrc'
 alias g='gvim'
+
+
+" vagrant
+alias vs="vagrant status"
+alias vu="vagrant up"
+alias vp="vagrant provision"
+
+
+" ssh keys
+# pipe public key to clipboard.
+alias pubkey="more ~/.ssh/id_dsa.public | pbcopy | echo '=> Public key copied to pasteboard.'"
+
+
+# grc overides for ls
+if $(gls &>/dev/null)
+then
+  alias ls="gls -F --color"
+  alias l="gls -lAh --color"
+  alias ll="gls -l --color"
+  alias la='gls -A --color'
+fi
+
+" ruby
+alias r='rvm use 1.9.3-head'
+
+alias f='RAILS_ENV=fi'
+
+alias sc='script/console'
+alias sg='script/generate'
+alias sd='script/destroy'
+
+alias migrate='rake db:migrate db:test:clone'
+
+
+# git
+alias gl='git pull --prune'
+alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
+alias gp='git push origin HEAD'
+alias gd='git diff'
+alias gc='git commit'
+alias gca='git commit -a'
+alias gco='git checkout'
+alias gb='git branch'
+alias gs='git status -sb'
+alias grm="git status | grep deleted | awk '{print \$3}' | xargs git rm"
+
+
+# todo.sh: https://github.com/ginatrapani/todo.txt-cli
+function t() { 
+  if [ $# -eq 0 ]; then
+    todo.sh ls
+  else
+    todo.sh $*
+  fi
+}
+
+alias n="t ls +next"
