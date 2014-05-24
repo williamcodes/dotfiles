9437a8d2b0e25336705f1fb8b8d6be48a40999f2
Dane O'Connor <dane.oconnor@gmail.com>
2014-05-24 12:34:48 -0400

    Only rename tmux window if we're in a tmux session

--- home/zsh/smart_tmux_title.zsh
+++ home/zsh/smart_tmux_title.zsh
@@ -1,13 +1,17 @@
 # Change tmux title to command name before executing
 function preexec() {
-  # strip bundle exec and only print first word of command
-  TITLE=$(echo $1 | sed 's/bundle exec//' | awk '{print $1;}')
-  CWD=$(basename $(pwd))
-  tmux rename-window "$CWD-$TITLE"
+  if [[ $TERM =~ screen ]]; then
+    # strip bundle exec and only print first word of command
+    TITLE=$(echo $1 | sed 's/bundle exec//' | awk '{print $1;}')
+    CWD=$(basename $(pwd))
+    tmux rename-window "$CWD-$TITLE"
+  fi
 }
 
 # Change tmux title to cwd after command
 function precmd() {
-  CWD=$(basename $(pwd))
-  tmux rename-window $CWD
+  if [[ $TERM =~ screen ]]; then
+    CWD=$(basename $(pwd))
+    tmux rename-window $CWD
+  fi
 }
