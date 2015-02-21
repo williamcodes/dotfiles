492f3546704f9394b8bb9b255ad57c0ebb95654a
Dane O'Connor <dane.oconnor@gmail.com>
2015-02-20 22:24:31 -0500

    Disable `smart_tmux_title.zsh`

--- home/zsh/smart_tmux_title.zsh
+++ home/zsh/smart_tmux_title.zsh
@@ -1,17 +1,17 @@
 # Change tmux title to command name before executing
-function preexec() {
-  if [[ $TERM =~ screen ]]; then
-    # strip bundle exec and only print first word of command
-    TITLE=$(echo $1 | sed 's/bundle exec//' | awk '{print $1;}')
-    CWD=$(basename $(pwd))
-    tmux rename-window "$CWD-$TITLE"
-  fi
-}
-
-# Change tmux title to cwd after command
-function precmd() {
-  if [[ $TERM =~ screen ]]; then
-    CWD=$(basename $(pwd))
-    tmux rename-window $CWD
-  fi
-}
+# function preexec() {
+#   if [[ $TERM =~ screen ]]; then
+#     # strip bundle exec and only print first word of command
+#     TITLE=$(echo $1 | sed 's/bundle exec//' | awk '{print $1;}')
+#     CWD=$(basename $(pwd))
+#     tmux rename-window "$CWD-$TITLE"
+#   fi
+# }
+#
+# # Change tmux title to cwd after command
+# function precmd() {
+#   if [[ $TERM =~ screen ]]; then
+#     CWD=$(basename $(pwd))
+#     tmux rename-window $CWD
+#   fi
+# }
